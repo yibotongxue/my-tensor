42d8e0e2bea8f2b6a4ee3aa6b6e73d846bed60e1
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-10-15 11:49:06 +0800

    Add sigmoid test

---
+++ test/sigmoid-test.cu
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include <layer.cuh>
+#include <sigmoid.cuh>
+#include <layer/layer-utils.cuh>
+
+#include <random>
+#include <algorithm>
+#include <ranges>
+
+class SigmoidTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    data.resize(30000);
+    diff.resize(30000);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis (-3.0f, 3.0f);
+    for (int i = 0; i < 30000; i++) {
+      data[i] = dis(gen);
+    }    
+    for (int i = 0; i < 30000; i++) {
+      diff[i] = dis(gen);
+    }
+    sigmoid.reset();
+    bottom.reset();
+    top.reset();
+    sigmoid = std::make_unique<my_tensor::Sigmoid<>>();
+    bottom = std::make_shared<my_tensor::Tensor<>>(shape);
+    top = std::make_shared<my_tensor::Tensor<>>(shape);
+    bottom->SetData(data);
+    top->SetDiff(diff);
+  }
+
+  const std::vector<int> shape {10000, 3};
+  std::vector<float> data;
+  std::vector<float> diff;
+  my_tensor::LayerPtr<> sigmoid;
+  my_tensor::TensorPtr<> bottom;
+  my_tensor::TensorPtr<> top;
+};
+
+TEST_F(SigmoidTest, Forward_Data) {
+  sigmoid->Forward(bottom, top);
+  const std::vector<float> top_data (top->GetData().begin(), top->GetData().end());
+  std::ranges::transform(data, data.begin(), [](float x) { return 1.0f / (1.0f + std::exp(-x)); });
+  for (int i = 0; i < 30000; i++) {
+    EXPECT_NEAR(top_data[i], data[i], 0.01);
+  }
+}
+
+BACKWARD_TEST(Sigmoid, sigmoid)
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
