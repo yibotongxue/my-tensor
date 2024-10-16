63053a95847b108b00bf49ddd5fcafa096406bf5
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-10-16 16:58:55 +0800

    Fix relu test

--- test/include/layer/layer-utils.cuh
+++ test/include/layer/layer-utils.cuh
@@ -5,29 +5,29 @@
 #include <ranges>
 #include <tuple>
 
-#define BACKWARD_TEST(layer_class, layer_name)                                                \
-  TEST_F(layer_class##Test, BackwardDiff)                                                     \
-  {                                                                                           \
-    layer_name->Forward(bottom, top);                                                         \
-    layer_name->Backward(top, bottom);                                                        \
-    std::vector<float> bottom_diff(bottom->GetDiff().begin(), bottom->GetDiff().end());       \
-    my_tensor::TensorPtr<> new_bottom = std::make_shared<my_tensor::Tensor<>>(shape);         \
-    std::vector<float> new_bottom_data(30000);                                                \
-    std::ranges::transform(data, new_bottom_data.begin(), [](float x) { return x + 0.001; }); \
-    new_bottom->SetData(new_bottom_data);                                                     \
-    my_tensor::TensorPtr<> new_top = std::make_shared<my_tensor::Tensor<>>(shape);            \
-    layer_name->Forward(new_bottom, new_top);                                                 \
-    std::vector<float> new_top_data(new_top->GetData().begin(), new_top->GetData().end());    \
-    const std::vector<float> top_data(top->GetData().begin(), top->GetData().end());          \
-    std::vector<float> results(new_top_data.size());                                          \
-                                                                                              \
-    std::transform(new_top_data.begin(), new_top_data.end(), top_data.begin(),                \
-                   results.begin(), [](float x, float y) { return (x - y) / 0.001f; });       \
-    std::ranges::transform(results, diff, results.begin(), std::multiplies<float>());         \
-    for (int i = 0; i < 30000; i++)                                                           \
-    {                                                                                         \
-      EXPECT_NEAR(results[i], bottom_diff[i], 0.01);                                          \
-    }                                                                                         \
+#define BACKWARD_TEST(layer_class, layer_name, device)                                                         \
+  TEST_F(layer_class##device##Test, BackwardDiff)                                                              \
+  {                                                                                                            \
+    layer_name->Forward##device(bottom, top);                                                                  \
+    layer_name->Backward##device(top, bottom);                                                                 \
+    std::vector<float> bottom_diff(bottom->Get##device##Diff().begin(), bottom->Get##device##Diff().end());    \
+    my_tensor::TensorPtr<> new_bottom = std::make_shared<my_tensor::Tensor<>>(shape);                          \
+    std::vector<float> new_bottom_data(30000);                                                                 \
+    std::ranges::transform(data, new_bottom_data.begin(), [](float x) { return x + 0.001; });                  \
+    new_bottom->Set##device##Data(new_bottom_data);                                                            \
+    my_tensor::TensorPtr<> new_top = std::make_shared<my_tensor::Tensor<>>(shape);                             \
+    layer_name->Forward##device(new_bottom, new_top);                                                          \
+    std::vector<float> new_top_data(new_top->Get##device##Data().begin(), new_top->Get##device##Data().end()); \
+    const std::vector<float> top_data(top->Get##device##Data().begin(), top->Get##device##Data().end());       \
+    std::vector<float> results(new_top_data.size());                                                           \
+                                                                                                               \
+    std::transform(new_top_data.begin(), new_top_data.end(), top_data.begin(),                                 \
+                   results.begin(), [](float x, float y) { return (x - y) / 0.001f; });                        \
+    std::ranges::transform(results, diff, results.begin(), std::multiplies<float>());                          \
+    for (int i = 0; i < 30000; i++)                                                                            \
+    {                                                                                                          \
+      EXPECT_NEAR(results[i], bottom_diff[i], 0.01);                                                           \
+    }                                                                                                          \
   }
 
 #endif // TEST_INCLUDE_LAYER_LAYER_UTILS_CUH_
--- test/relu-test.cu
+++ test/relu-test.cu
@@ -13,57 +13,74 @@
 //   EXPECT_TRUE(false);
 // }
 
-class ReluTest : public ::testing::Test {
- protected:
-  void SetUp() override {
-    data.resize(30000);
-    diff.resize(30000);
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dis (-3.0f, 3.0f);
-    for (int i = 0; i < 30000; i++) {
-      data[i] = dis(gen);
-      if (data[i] >= -0.001 && data[i] <= 0) {
-        data[i] = 0.001;
-      }
-    }    
-    for (int i = 0; i < 30000; i++) {
-      diff[i] = dis(gen);
-    }
-    relu.reset();
-    bottom.reset();
-    top.reset();
-    relu = std::make_unique<my_tensor::Relu<>>();
-    bottom = std::make_shared<my_tensor::Tensor<>>(shape);
-    top = std::make_shared<my_tensor::Tensor<>>(shape);
-    bottom->SetData(data);
-    top->SetDiff(diff);
+#define RELU_TEST_CLASS(device)                               \
+  class Relu##device##Test : public ::testing::Test           \
+  {                                                           \
+  protected:                                                  \
+    void SetUp() override                                     \
+    {                                                         \
+      data.resize(30000);                                     \
+      diff.resize(30000);                                     \
+      std::random_device rd;                                  \
+      std::mt19937 gen(rd());                                 \
+      std::uniform_real_distribution<float> dis(-3.0f, 3.0f); \
+      for (int i = 0; i < 30000; i++)                         \
+      {                                                       \
+        data[i] = dis(gen);                                   \
+        if (data[i] >= -0.001 && data[i] <= 0)                \
+        {                                                     \
+          data[i] = 0.001;                                    \
+        }                                                     \
+      }                                                       \
+      for (int i = 0; i < 30000; i++)                         \
+      {                                                       \
+        diff[i] = dis(gen);                                   \
+      }                                                       \
+      relu.reset();                                           \
+      bottom.reset();                                         \
+      top.reset();                                            \
+      relu = std::make_unique<my_tensor::Relu<>>();           \
+      bottom = std::make_shared<my_tensor::Tensor<>>(shape);  \
+      top = std::make_shared<my_tensor::Tensor<>>(shape);     \
+      bottom->Set##device##Data(data);                        \
+      top->Set##device##Diff(diff);                           \
+    }                                                         \
+    const std::vector<int> shape{10000, 3};                   \
+    std::vector<float> data;                                  \
+    std::vector<float> diff;                                  \
+    my_tensor::LayerPtr<> relu;                               \
+    my_tensor::TensorPtr<> bottom;                            \
+    my_tensor::TensorPtr<> top;                               \
+  };
+
+RELU_TEST_CLASS(CPU)
+RELU_TEST_CLASS(GPU)
+
+#define RELU_FORWARD_TEST(device)                                                                        \
+  TEST_F(Relu##device##Test, Forward_Data)                                                               \
+  {                                                                                                      \
+    relu->Forward##device(bottom, top);                                                                  \
+    const std::vector<float> top_data(top->Get##device##Data().begin(), top->Get##device##Data().end()); \
+    for (int i = 0; i < 30000; i++)                                                                      \
+    {                                                                                                    \
+      if (data[i] > 0)                                                                                   \
+      {                                                                                                  \
+        EXPECT_EQ(top_data[i], data[i]);                                                                 \
+      }                                                                                                  \
+      else                                                                                               \
+      {                                                                                                  \
+        EXPECT_EQ(top_data[i], 0);                                                                       \
+      }                                                                                                  \
+    }                                                                                                    \
   }
 
+RELU_FORWARD_TEST(CPU)
+RELU_FORWARD_TEST(GPU)
+BACKWARD_TEST(Relu, relu, CPU)
+BACKWARD_TEST(Relu, relu, GPU)
 
-  const std::vector<int> shape {10000, 3};
-  std::vector<float> data;
-  std::vector<float> diff;
-  my_tensor::LayerPtr<> relu;
-  my_tensor::TensorPtr<> bottom;
-  my_tensor::TensorPtr<> top;
-};
-
-TEST_F(ReluTest, Forward_Data) {
-  relu->Forward(bottom, top);
-  const std::vector<float> top_data (top->GetData().begin(), top->GetData().end());
-  for (int i = 0; i < 30000; i++) {
-    if (data[i] > 0) {
-      EXPECT_EQ(top_data[i], data[i]);
-    } else {
-      EXPECT_EQ(top_data[i], 0);
-    }
-  }
-}
-
-BACKWARD_TEST(Relu, relu)
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
