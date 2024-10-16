f332f9178d1a23f6486d8e9828483f1c18772b5c
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-10-16 17:02:35 +0800

    FIx sigmoid test

--- test/sigmoid-test.cu
+++ test/sigmoid-test.cu
@@ -7,50 +7,65 @@
 #include <algorithm>
 #include <ranges>
 
-class SigmoidTest : public ::testing::Test {
- protected:
-  void SetUp() override {
-    data.resize(30000);
-    diff.resize(30000);
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dis (-3.0f, 3.0f);
-    for (int i = 0; i < 30000; i++) {
-      data[i] = dis(gen);
-    }    
-    for (int i = 0; i < 30000; i++) {
-      diff[i] = dis(gen);
-    }
-    sigmoid.reset();
-    bottom.reset();
-    top.reset();
-    sigmoid = std::make_unique<my_tensor::Sigmoid<>>();
-    bottom = std::make_shared<my_tensor::Tensor<>>(shape);
-    top = std::make_shared<my_tensor::Tensor<>>(shape);
-    bottom->SetData(data);
-    top->SetDiff(diff);
+#define SIGMOID_TEST_CLASS(device)                            \
+  class Sigmoid##device##Test : public ::testing::Test        \
+  {                                                           \
+  protected:                                                  \
+    void SetUp() override                                     \
+    {                                                         \
+      data.resize(30000);                                     \
+      diff.resize(30000);                                     \
+      std::random_device rd;                                  \
+      std::mt19937 gen(rd());                                 \
+      std::uniform_real_distribution<float> dis(-3.0f, 3.0f); \
+      for (int i = 0; i < 30000; i++)                         \
+      {                                                       \
+        data[i] = dis(gen);                                   \
+      }                                                       \
+      for (int i = 0; i < 30000; i++)                         \
+      {                                                       \
+        diff[i] = dis(gen);                                   \
+      }                                                       \
+      sigmoid.reset();                                        \
+      bottom.reset();                                         \
+      top.reset();                                            \
+      sigmoid = std::make_unique<my_tensor::Sigmoid<>>();     \
+      bottom = std::make_shared<my_tensor::Tensor<>>(shape);  \
+      top = std::make_shared<my_tensor::Tensor<>>(shape);     \
+      bottom->Set##device##Data(data);                        \
+      top->Set##device##Diff(diff);                           \
+    }                                                         \
+    const std::vector<int> shape{10000, 3};                   \
+    std::vector<float> data;                                  \
+    std::vector<float> diff;                                  \
+    my_tensor::LayerPtr<> sigmoid;                            \
+    my_tensor::TensorPtr<> bottom;                            \
+    my_tensor::TensorPtr<> top;                               \
+  };
+
+SIGMOID_TEST_CLASS(CPU)
+SIGMOID_TEST_CLASS(GPU)
+
+#define SIGMOID_FORWARD_TEST(device)                                                                     \
+  TEST_F(Sigmoid##device##Test, Forward_Data)                                                            \
+  {                                                                                                      \
+    sigmoid->Forward##device(bottom, top);                                                               \
+    const std::vector<float> top_data(top->Get##device##Data().begin(), top->Get##device##Data().end()); \
+    std::ranges::transform(data, data.begin(), [](float x) { return 1.0f / (1.0f + std::exp(-x)); });    \
+    for (int i = 0; i < 30000; i++)                                                                      \
+    {                                                                                                    \
+      EXPECT_NEAR(top_data[i], data[i], 0.01);                                                           \
+    }                                                                                                    \
   }
 
-  const std::vector<int> shape {10000, 3};
-  std::vector<float> data;
-  std::vector<float> diff;
-  my_tensor::LayerPtr<> sigmoid;
-  my_tensor::TensorPtr<> bottom;
-  my_tensor::TensorPtr<> top;
-};
+SIGMOID_FORWARD_TEST(CPU)
+SIGMOID_FORWARD_TEST(GPU)
 
-TEST_F(SigmoidTest, Forward_Data) {
-  sigmoid->Forward(bottom, top);
-  const std::vector<float> top_data (top->GetData().begin(), top->GetData().end());
-  std::ranges::transform(data, data.begin(), [](float x) { return 1.0f / (1.0f + std::exp(-x)); });
-  for (int i = 0; i < 30000; i++) {
-    EXPECT_NEAR(top_data[i], data[i], 0.01);
-  }
-}
+BACKWARD_TEST(Sigmoid, sigmoid, CPU)
+BACKWARD_TEST(Sigmoid, sigmoid, GPU)
 
-BACKWARD_TEST(Sigmoid, sigmoid)
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
