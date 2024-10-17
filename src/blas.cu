ded99ed9ee3fb31fe2e2e4751a333fc47b35b070
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-10-17 10:35:24 +0800

    Add comment and fix exception expression

--- src/blas.cu
+++ src/blas.cu
@@ -9,7 +9,7 @@ template <>
 Tensor<> operator+(const Tensor<>& lhs, const Tensor<>& rhs) {
   int n = lhs.GetSize();
   if (rhs.GetSize() != n) {
-    throw BlasError("Tensor operator + size not match.");
+    throw BlasError("Tensor operator + sizes not match.");
   }
   Tensor<> result = lhs;
   float alpha = 1.0f;
@@ -23,7 +23,7 @@ Tensor<> matmul(const Tensor<>& lhs, const Tensor<>& rhs) {
   const auto& left_shape = lhs.GetShape();
   const auto& right_shape = rhs.GetShape();
   if (left_shape.size() != 2 || right_shape.size() != 2 || left_shape[1] != right_shape[0]) {
-    throw BlasError("Tensor matmul shape not match.");
+    throw BlasError("Tensor matmul shapes not match.");
   }
   int m = left_shape[0], k = left_shape[1], n = right_shape[1];
   Tensor<> result({m, n});
@@ -31,7 +31,7 @@ Tensor<> matmul(const Tensor<>& lhs, const Tensor<>& rhs) {
   float beta = 0.0f;
   // result<sup>T</sup> = rhs<sup>T</sup>lhs<sup>T</sup>
   // also result = (lhs)(rhs)
-  CUBLAS_ERROR_CHECK(cublasSgemm(handle->GetHandle(),
+  CUBLAS_ERROR_CHECK(cublasSgemm(handle->GetHandle(),  // handle
     CUBLAS_OP_N,  // no transpose of lhs<sup>T</sup>
     CUBLAS_OP_N,  // no transpose of rhs<sup>T</sup>
     n,  // row number of rhs<sup>T</sup> and row number of result<sup>T</sup>
