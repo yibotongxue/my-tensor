870282c0e40be20b387ab06bf61f5a0f2a0952ab
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-10-23 22:10:13 +0800

    Add im2col gpu

--- src/im2col.cu
+++ src/im2col.cu
@@ -128,11 +128,47 @@ void Im2col_GPU(const int n, const T *data_im, const int channels,
   );
 }
 
+namespace {
+template <typename T>
+__global__ void Col2im_kernel(const T *data_col, const int kernel_nums,
+    const int height, const int width, const int kernel_h,
+    const int kernel_w, T *data_im, const int im_size,
+    const int col_size) {
+  CUDA_KERNEL_LOOP(index, kernel_nums) {
+    int channel_index = index / im_size;
+    int w_index = index % im_size;
+    int write_h = w_index / width;
+    int write_w = w_index % width;
+    int h_offset = write_h + (kernel_h - 1) / 2;
+    int w_offset = write_w + (kernel_w - 1) / 2;
+    T val = 0;
+    const T *data_read = data_col + channel_index * col_size;
+    for (int kernel_row = 0; kernel_row < kernel_h; kernel_row++) {
+      for (int kernel_col = 0; kernel_col < kernel_w; kernel_col++) {
+        int read_h = h_offset - kernel_row;
+        int read_w = w_offset - kernel_col;
+        if (read_h >= 0 && read_h < height && read_w >= 0 && read_w < width) {
+          val += data_read[read_h * width + read_w];
+        }
+        data_read += im_size;
+      }
+    }
+    data_im[index] = val;
+  }
+}
+}  // namespace
+
 template <typename T>
 void Col2im_GPU(const int n, const T *data_col, const int channels,
     const int height, const int width, const int kernel_h,
     const int kernel_w, T *data_im) {
   CHECK_KERNEL_SHAPE
+  int im_size = height * width;
+  int kernel_nums = n * channels * im_size;
+  int col_size = kernel_w * kernel_h * im_size;
+  Col2im_kernel<<<CudaGetBlocks(kernel_nums), kCudaThreadNum>>>(
+      data_col, kernel_nums, height, width, kernel_h, kernel_w,
+      data_im, im_size, col_size);
 }
 
 template void Im2col_CPU<float>(const int n, const float *data_im,
