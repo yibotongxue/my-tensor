bf508b904096de8c6aabe01709d952d396e69b40
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-10-26 13:14:29 +0800

    Format utils.cuh

--- include/utils.cuh
+++ include/utils.cuh
@@ -1,12 +1,13 @@
+// Copyright 2024 yibotongxue
+
 #ifndef INCLUDE_UTILS_CUH_
 #define INCLUDE_UTILS_CUH_
 
-#include <stdio.h>
+#include <iostream>
 
 constexpr int kCudaThreadNum = 512;
 
-inline int CudaGetBlocks(const int N)
-{
+inline int CudaGetBlocks(const int N) {
   return (N + kCudaThreadNum - 1) / kCudaThreadNum;
 }
 
@@ -15,46 +16,42 @@ inline int CudaGetBlocks(const int N)
        i += blockDim.x * gridDim.x)
 
 #ifdef DEBUG
-#define ERROR_CHECK(function)                                                   \
-  do                                                                            \
-  {                                                                             \
-    cudaError_t error_code = function;                                          \
-    if (error_code != cudaSuccess)                                              \
-    {                                                                           \
-      std::cerr << "CUDA error:\r\ncode = " << error_code                       \
-                << ", name = " << cudaGetErrorName(error_code)                  \
-                << ", description = " << cudaGetErrorString(error_code)         \
-                << "\r\nfile = " << __FILE__ << ", line" << __LINE__ << "\r\n"; \
-    }                                                                           \
+#define ERROR_CHECK(function)                                           \
+  do {                                                                  \
+    cudaError_t error_code = function;                                  \
+    if (error_code != cudaSuccess) {                                    \
+      std::cerr << "CUDA error:\r\ncode = " << error_code               \
+                << ", name = " << cudaGetErrorName(error_code)          \
+                << ", description = " << cudaGetErrorString(error_code) \
+                << "\r\nfile = " << __FILE__ << ", line" << __LINE__    \
+                << "\r\n";                                              \
+    }                                                                   \
   } while (0);
 #else
 #define ERROR_CHECK(function) function
-#endif // DEBUG
+#endif  // DEBUG
 
 #ifdef DEBUG
-#define CUBLAS_ERROR_CHECK(function)                                            \
-  do                                                                            \
-  {                                                                             \
-    cublasStatus_t status = function;                                           \
-    if (status != CUBLAS_STATUS_SUCCESS)                                        \
-    {                                                                           \
-      std::cerr << "CuBLAS error:\r\nstatus = " << status                       \
-                << ", name = " << cublasGetStatusName(status)                   \
-                << ", description = " << cublasGetStatusString(status)          \
-                << "\r\nfile = " << __FILE__ << ", line" << __LINE__ << "\r\n"; \
-    }                                                                           \
+#define CUBLAS_ERROR_CHECK(function)                                   \
+  do {                                                                 \
+    cublasStatus_t status = function;                                  \
+    if (status != CUBLAS_STATUS_SUCCESS) {                             \
+      std::cerr << "CuBLAS error:\r\nstatus = " << status              \
+                << ", name = " << cublasGetStatusName(status)          \
+                << ", description = " << cublasGetStatusString(status) \
+                << "\r\nfile = " << __FILE__ << ", line" << __LINE__   \
+                << "\r\n";                                             \
+    }                                                                  \
   } while (0);
 #else
 #define CUBLAS_ERROR_CHECK(function) function
-#endif // DEBUG
+#endif  // DEBUG
 
 #define CHECK_GPU_AVAILABLE                                      \
-  do                                                             \
-  {                                                              \
+  do {                                                           \
     int device_count = 0;                                        \
     cudaError_t error = cudaGetDeviceCount(&device_count);       \
-    if (error != cudaSuccess || device_count == 0)               \
-    {                                                            \
+    if (error != cudaSuccess || device_count == 0) {             \
       std::cerr << "No CUDA campatable GPU found!" << std::endl; \
       throw std::runtime_error("No CUDA campatable GPU found!"); \
     }                                                            \
@@ -66,13 +63,11 @@ inline int CudaGetBlocks(const int N)
   layer_name(layer_name<T> &&) = delete;                    \
   layer_name<T> &operator=(layer_name<T> &&) = delete;
 
-#define CHECK_SAME_SHAPE(tensor, another)          \
-  do                                               \
-  {                                                \
-    if (tensor->GetShape() != another->GetShape()) \
-    {                                              \
-      throw ShapeError("Shape not match.");        \
-    }                                              \
+#define CHECK_SAME_SHAPE(tensor, another)            \
+  do {                                               \
+    if (tensor->GetShape() != another->GetShape()) { \
+      throw ShapeError("Shape not match.");          \
+    }                                                \
   } while (0);
 
 #define BLAS_UNIMPLEMENTION throw BlasError("Unimplemention error.");
@@ -80,13 +75,13 @@ inline int CudaGetBlocks(const int N)
 #define IM2COL_UNIMPLEMENTION throw Im2colError("Unimplemention error.");
 
 #define CHECK_KERNEL_SHAPE                          \
-  if ((kernel_h % 2 == 0) || (kernel_w % 2 == 0))   \
-  {                                                 \
+  if ((kernel_h % 2 == 0) || (kernel_w % 2 == 0)) { \
     throw Im2colError("Kernel shape not be even."); \
   }
 
-#define AT_GRAD_GPU_DATA(tensor) at_grad ? tensor.GetGPUDiffPtr() : tensor.GetGPUDataPtr()
+#define AT_GRAD_GPU_DATA(tensor) \
+  at_grad ? tensor.GetGPUDiffPtr() : tensor.GetGPUDataPtr()
 
 #define RAW_PTR(vec) thrust::raw_pointer_cast(vec.data())
 
-#endif // INCLUDE_UTILS_CUH_
+#endif  // INCLUDE_UTILS_CUH_
