c9ae0245a7ea10c5412bc73f77fdab7f780b4b6b
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-10-26 13:33:59 +0800

    Format

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
+// Copyright 2024 yibotongxue
+
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from my_tensor!\n";
-}
+int main(int, char**) { std::cout << "Hello, from my_tensor!\n"; }
