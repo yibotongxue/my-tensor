f96f8f2a5f878a86ba5b0327b02c10d1bb6682bc
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2024-10-26 13:32:39 +0800

    Format

--- test/include/layer/layer-utils.cuh
+++ test/include/layer/layer-utils.cuh
@@ -1,33 +1,44 @@
+// Copyright 2024 yibotongxue
+
 #ifndef TEST_INCLUDE_LAYER_LAYER_UTILS_CUH_
 #define TEST_INCLUDE_LAYER_LAYER_UTILS_CUH_
 
 #include <algorithm>
+#include <functional>
+#include <memory>
 #include <ranges>
 #include <tuple>
+#include <vector>
 
-#define BACKWARD_TEST(layer_class, layer_name, device)                                                         \
-  TEST_F(layer_class##device##Test, BackwardDiff)                                                              \
-  {                                                                                                            \
-    layer_name->Forward##device(bottom, top);                                                                  \
-    layer_name->Backward##device(top, bottom);                                                                 \
-    std::vector<float> bottom_diff(bottom->Get##device##Diff().begin(), bottom->Get##device##Diff().end());    \
-    my_tensor::TensorPtr<> new_bottom = std::make_shared<my_tensor::Tensor<>>(shape);                          \
-    std::vector<float> new_bottom_data(30000);                                                                 \
-    std::ranges::transform(data, new_bottom_data.begin(), [](float x) { return x + 0.001; });                  \
-    new_bottom->Set##device##Data(new_bottom_data);                                                            \
-    my_tensor::TensorPtr<> new_top = std::make_shared<my_tensor::Tensor<>>(shape);                             \
-    layer_name->Forward##device(new_bottom, new_top);                                                          \
-    std::vector<float> new_top_data(new_top->Get##device##Data().begin(), new_top->Get##device##Data().end()); \
-    const std::vector<float> top_data(top->Get##device##Data().begin(), top->Get##device##Data().end());       \
-    std::vector<float> results(new_top_data.size());                                                           \
-                                                                                                               \
-    std::transform(new_top_data.begin(), new_top_data.end(), top_data.begin(),                                 \
-                   results.begin(), [](float x, float y) { return (x - y) / 0.001f; });                        \
-    std::ranges::transform(results, diff, results.begin(), std::multiplies<float>());                          \
-    for (int i = 0; i < 30000; i++)                                                                            \
-    {                                                                                                          \
-      EXPECT_NEAR(results[i], bottom_diff[i], 0.01);                                                           \
-    }                                                                                                          \
+#define BACKWARD_TEST(layer_class, layer_name, device)                         \
+  TEST_F(layer_class##device##Test, BackwardDiff) {                            \
+    layer_name->Forward##device(bottom, top);                                  \
+    layer_name->Backward##device(top, bottom);                                 \
+    std::vector<float> bottom_diff(bottom->Get##device##Diff().begin(),        \
+                                   bottom->Get##device##Diff().end());         \
+    my_tensor::TensorPtr<> new_bottom =                                        \
+        std::make_shared<my_tensor::Tensor<>>(shape);                          \
+    std::vector<float> new_bottom_data(30000);                                 \
+    std::ranges::transform(data, new_bottom_data.begin(),                      \
+                           [](float x) { return x + 0.001; });                 \
+    new_bottom->Set##device##Data(new_bottom_data);                            \
+    my_tensor::TensorPtr<> new_top =                                           \
+        std::make_shared<my_tensor::Tensor<>>(shape);                          \
+    layer_name->Forward##device(new_bottom, new_top);                          \
+    std::vector<float> new_top_data(new_top->Get##device##Data().begin(),      \
+                                    new_top->Get##device##Data().end());       \
+    const std::vector<float> top_data(top->Get##device##Data().begin(),        \
+                                      top->Get##device##Data().end());         \
+    std::vector<float> results(new_top_data.size());                           \
+                                                                               \
+    std::transform(new_top_data.begin(), new_top_data.end(), top_data.begin(), \
+                   results.begin(),                                            \
+                   [](float x, float y) { return (x - y) / 0.001f; });         \
+    std::ranges::transform(results, diff, results.begin(),                     \
+                           std::multiplies<float>());                          \
+    for (int i = 0; i < 30000; i++) {                                          \
+      EXPECT_NEAR(results[i], bottom_diff[i], 0.01);                           \
+    }                                                                          \
   }
 
-#endif // TEST_INCLUDE_LAYER_LAYER_UTILS_CUH_
+#endif  // TEST_INCLUDE_LAYER_LAYER_UTILS_CUH_
--- test/include/tensor/tensor-utils.cuh
+++ test/include/tensor/tensor-utils.cuh
@@ -1,38 +1,37 @@
-#ifndef TEST_INCLUDE_TEST_UTILS_CUH_
-#define TEST_INCLUDE_TEST_UTILS_CUH_
+// Copyright 2024 yibotongxue
 
-#define TEST_SHAPE_AND_SIZE(test_fixture)                       \
-  TEST_F(test_fixture##Test, test_fixture##_ShapeMatches)       \
-  {                                                             \
-    const std::vector<int> shape = {2, 3};                      \
-    EXPECT_EQ(tensor->GetShape(), shape);                       \
-  }                                                             \
-  TEST_F(test_fixture##Test, test_fixture##_SizeMatches)        \
-  {                                                             \
-    EXPECT_EQ(tensor->GetSize(), 6);                            \
-  }                                                             \
-  TEST_F(test_fixture##Test, test_fixture##_CPUDataSizeMatches) \
-  {                                                             \
-    EXPECT_EQ(tensor->GetCPUData().size(), 6);                  \
-  }                                                             \
-  TEST_F(test_fixture##Test, test_fixture##_CPUDiffSizeMatches) \
-  {                                                             \
-    EXPECT_EQ(tensor->GetCPUDiff().size(), 6);                  \
-  }                                                             \
-  TEST_F(test_fixture##Test, test_fixture##_GPUDataSizeMatches) \
-  {                                                             \
-    EXPECT_EQ(tensor->GetGPUData().size(), 6);                  \
-  }                                                             \
-  TEST_F(test_fixture##Test, test_fixture##_GPUDiffSizeMatches) \
-  {                                                             \
-    EXPECT_EQ(tensor->GetGPUDiff().size(), 6);                  \
+#ifndef TEST_INCLUDE_TENSOR_TENSOR_UTILS_CUH_
+#define TEST_INCLUDE_TENSOR_TENSOR_UTILS_CUH_
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#define TEST_SHAPE_AND_SIZE(test_fixture)                         \
+  TEST_F(test_fixture##Test, test_fixture##_ShapeMatches) {       \
+    const std::vector<int> shape = {2, 3};                        \
+    EXPECT_EQ(tensor->GetShape(), shape);                         \
+  }                                                               \
+  TEST_F(test_fixture##Test, test_fixture##_SizeMatches) {        \
+    EXPECT_EQ(tensor->GetSize(), 6);                              \
+  }                                                               \
+  TEST_F(test_fixture##Test, test_fixture##_CPUDataSizeMatches) { \
+    EXPECT_EQ(tensor->GetCPUData().size(), 6);                    \
+  }                                                               \
+  TEST_F(test_fixture##Test, test_fixture##_CPUDiffSizeMatches) { \
+    EXPECT_EQ(tensor->GetCPUDiff().size(), 6);                    \
+  }                                                               \
+  TEST_F(test_fixture##Test, test_fixture##_GPUDataSizeMatches) { \
+    EXPECT_EQ(tensor->GetGPUData().size(), 6);                    \
+  }                                                               \
+  TEST_F(test_fixture##Test, test_fixture##_GPUDiffSizeMatches) { \
+    EXPECT_EQ(tensor->GetGPUDiff().size(), 6);                    \
   }
 
 #define DEFINE_DATA_AND_DIFF(data_name, diff_name) \
   data_name.resize(6);                             \
   diff_name.resize(6);                             \
-  for (int i = 0; i < 6; i++)                      \
-  {                                                \
+  for (int i = 0; i < 6; i++) {                    \
     data_name[i] = i + 1;                          \
   }                                                \
   diff_name[0] = 1;                                \
@@ -44,19 +43,15 @@
 
 #define DEFINE_TESNOR(tensor_name)                                            \
   const std::vector<int> tensor_name##_shape{2, 3};                           \
-  try                                                                         \
-  {                                                                           \
+  try {                                                                       \
     tensor_name = std::make_shared<my_tensor::Tensor<>>(tensor_name##_shape); \
-  }                                                                           \
-  catch (my_tensor::ShapeError & e)                                           \
-  {                                                                           \
+  } catch (my_tensor::ShapeError & e) {                                       \
     std::cerr << e.what() << std::endl;                                       \
     FAIL() << "Failed to construct tensor.";                                  \
   }
 
 #define DATA_EQUAL_TEST(device)                       \
-  for (int i = 0; i < 6; i++)                         \
-  {                                                   \
+  for (int i = 0; i < 6; i++) {                       \
     EXPECT_EQ(tensor->Get##device##Data()[i], i + 1); \
   }
 
@@ -68,24 +63,22 @@
   EXPECT_EQ(tensor->Get##device##Diff()[4], 4); \
   EXPECT_EQ(tensor->Get##device##Diff()[5], 6);
 
-#define TEST_DATA(test_fixture, device)                \
-  TEST_F(test_fixture##Test, test_fixture##device##_DataEqual) \
-  {                                                    \
-    DATA_EQUAL_TEST(device)                            \
+#define TEST_DATA(test_fixture, device)                          \
+  TEST_F(test_fixture##Test, test_fixture##device##_DataEqual) { \
+    DATA_EQUAL_TEST(device)                                      \
   }
 
-#define TEST_DIFF(test_fixture, device)                \
-  TEST_F(test_fixture##Test, test_fixture##device##_DiffEqual) \
-  {                                                    \
-    DIFF_EQUAL_TEST(device)                            \
+#define TEST_DIFF(test_fixture, device)                          \
+  TEST_F(test_fixture##Test, test_fixture##device##_DiffEqual) { \
+    DIFF_EQUAL_TEST(device)                                      \
   }
 
 #define TEST_DATA_AND_DIFF_WITH_DEVICE(test_fixture, device) \
   TEST_DATA(test_fixture, device)                            \
   TEST_DIFF(test_fixture, device)
 
-#define TEST_DATA_AND_DIFF(test_fixture) \
-  TEST_DATA_AND_DIFF_WITH_DEVICE(test_fixture, CPU)    \
+#define TEST_DATA_AND_DIFF(test_fixture)            \
+  TEST_DATA_AND_DIFF_WITH_DEVICE(test_fixture, CPU) \
   TEST_DATA_AND_DIFF_WITH_DEVICE(test_fixture, GPU)
 
-#endif // TEST_INCLUDE_TEST_UTILS_CUH_
+#endif  // TEST_INCLUDE_TENSOR_TENSOR_UTILS_CUH_
