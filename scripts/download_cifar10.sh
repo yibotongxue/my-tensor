0aafd00f16c917bb3fc78fa8251b62517acc04b9
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2025-01-20 16:29:46 +0800

    Remove python files

--- scripts/download_cifar10.sh
+++ scripts/download_cifar10.sh
@@ -1,9 +1,3 @@
-#########################################################################
-# File Name:    scripts/download_cifar10.sh
-# Author:       林毅波
-# mail:         linyibo_2024@163.com
-# Created Time: Mon 20 Jan 2025 01:04:41 PM CST
-#########################################################################
 #!/bin/bash
 
 wget http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz -P data
