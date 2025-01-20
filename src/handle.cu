86acc4158f264adf7d7415a772a93098f1772482
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2025-01-20 17:44:33 +0800

    Remove handle

--- src/handle.cu
+++
@@ -1,9 +0,0 @@
-// Copyright 2024 yibotongxue
-
-#include "handle.hpp"
-
-namespace my_tensor {
-HandlePtr Handle::handle_ = nullptr;
-
-my_tensor::HandlePtr handle = my_tensor::Handle::GetInstance();
-}  // namespace my_tensor
